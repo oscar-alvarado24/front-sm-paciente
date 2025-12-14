4dd816be09edfa4c2a6a4da88177a7b38b058142
oscar alvarado <proyectos.oscar-alvarado@outlook.com>
2025-12-14 10:29:44 -0500

    se eliminan los datos de los proxys generados para su comsumo en local ya que se inician las pruebas con backend desplegado en aws

--- proxy.conf.cjs
+++ proxy.conf.cjs
@@ -1,29 +1,2 @@
 module.exports = {
-  "/api/procedure": {
-    "target": "http://127.0.0.1:8070",
-    "secure": false,
-    "changeOrigin": true,
-    "logLevel": "debug"
-  },
-  "/api/employee": {
-    "target": "http://127.0.0.1:3001",
-    "secure": false,
-    "changeOrigin": true,
-    "logLevel": "debug",
-    "pathRewrite": {
-      "^/api/employee": "/api/v1/employee"
-    }
-  },
-  "/providers": {
-    "target": "http://127.0.0.1:8050",
-    "secure": false,
-    "changeOrigin": true,
-    "logLevel": "debug",
-    "onProxyReq": (proxyReq, req, res) => {
-         console.log('Proxying:', req.method, req.url);
-    },
-       "onError": (err, req, res) => {
-         console.log('Proxy error:', err);
-    }
-  }
 }
