e055050ed422f344dbcf70afc5635758899546e5
oscar alvarado <proyectos.oscar-alvarado@outlook.com>
2026-01-22 19:08:08 -0500

    Al convertir el componente app.component en standalone se deben importar los componentes usados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './general/components/header/header.component';
+import { FooterComponent } from './general/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
