1c62cc2254eaa6051ce1e1671cc62c222cda8b53
oscar alvarado <proyectos.oscar-alvarado@outlook.com>
2026-01-24 20:27:58 -0500

    se cambia el archivo de js a mjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,4 @@
-// jest.config.js
-module.exports = {
+export default {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/e2e/'],
